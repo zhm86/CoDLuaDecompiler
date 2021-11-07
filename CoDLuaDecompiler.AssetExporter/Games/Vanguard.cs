9be22a736300b7e6e61aed35737cdf218fdd68b1
JariKCoding <jerriisgaming@gmail.com>
2021-11-07 14:46:15 +0100

    Removed import that i forgot

--- CoDLuaDecompiler.AssetExporter/Games/Vanguard.cs
+++ CoDLuaDecompiler.AssetExporter/Games/Vanguard.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using Newtonsoft.Json;
 using Formatting = System.Xml.Formatting;
 
 namespace CoDLuaDecompiler.AssetExporter.Games
